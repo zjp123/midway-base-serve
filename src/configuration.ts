97b3378278fc2c1524fe353d68c732612f8d01ac
zhangjunpeng <zhangjunpeng@58.com>
2024-03-28 17:44:59 +0800

    first commit

---
+++ src/configuration.ts
@@ -0,0 +1,14 @@
+import { Configuration, App } from '@midwayjs/core';
+import * as express from '@midwayjs/express';
+import { join } from 'path';
+
+@Configuration({
+  imports: [express],
+  importConfigs: [join(__dirname, './config')],
+})
+export class MainConfiguration {
+  @App('express')
+  app: express.Application;
+
+  async onReady() {}
+}
