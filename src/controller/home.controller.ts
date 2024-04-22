e95ab288938e4d7194232be30937e8d90bbb034a
zhangjunpeng <zhangjunpeng@58.com>
2024-04-22 19:09:14 +0800

    feat: 写了一点点

--- src/controller/home.controller.ts
+++ src/controller/home.controller.ts
@@ -18,7 +18,7 @@ export class HomeController {
     return 'Hello Midwayjs!'
   }
   async login(): Promise<string> {
-    const { username, password } = this.ctx.body
+    // const { username, password } = this.ctx.body
     // const user = this.userService[username]
     // if (!user || !(await bcrypt.compare(password, user.password))) {
     //   return res.status(401).send('Invalid credentials');
@@ -26,6 +26,6 @@ export class HomeController {
     // // 设置session
     // req.session.userId = username; // 存储用户唯一标识
     // req.session.loggedIn = true;
-    // res.send('Logged in successfully');
+    this.res.send('Logged in successfully')
   }
 }
