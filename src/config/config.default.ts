44349fba52ce759c47083a2771ed73003fda0254
zhangjunpeng <zhangjunpeng@58.com>
2024-04-23 22:10:32 +0800

    feat: 没写什么

--- src/config/config.default.ts
+++ src/config/config.default.ts
@@ -2,7 +2,7 @@ import { MidwayConfig } from '@midwayjs/core';
 
 export default {
   // use for cookie sign key, should change to your own and keep security
-  keys: '1711617627132_7411',
+  keys: ['2d_g', 'gk6_8'],
   express: {
     port: 7001,
   },
