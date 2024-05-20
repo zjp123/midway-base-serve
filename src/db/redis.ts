aabe6d58763829895d61bf392a97b0612bba2af7
zhangjunpeng <zhangjunpeng@58.com>
2024-05-20 19:17:41 +0800

    feat: redis设置密码

--- src/db/redis.ts
+++ src/db/redis.ts
@@ -1,15 +1,24 @@
 import IORedis from 'ioredis'
 
+// const redisClient = new IORedis({
+//   port: 6379, // Redis 端口号
+//   host: '127.0.0.1', // Redis 服务器的IP地址
+//   family: 4, // 4 (IPv4) 或者 6 (IPv6)
+//   password: 'auth', // 如果设置了连接密码
+//   db: 0,
+// })
+
 const redisClient = new IORedis({
   port: 6379, // Redis 端口号
   host: '127.0.0.1', // Redis 服务器的IP地址
   family: 4, // 4 (IPv4) 或者 6 (IPv6)
-  password: 'auth', // 如果设置了连接密码
+  password: 'zjp_123_456', // 如果设置了连接密码
   db: 0,
 })
+
 // 连接成功
 redisClient.on('connect', () => {
-  console.log('redis连接成功')
+  console.log('redis开始连接')
 })
 
 // 连接错误
@@ -24,6 +33,11 @@ redisClient.on('close', () => {
 // 尝试重新连接前触发
 redisClient.on('reconnecting', delay => {
   console.log(`redis正在尝试重新连接... 将在 ${delay} 毫秒后重连`)
+})
+
+// 监听连接成功事件
+redisClient.on('ready', function () {
+  console.log('redis 连接成功')
 })
 
 export default redisClient
